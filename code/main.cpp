d5b12cf27c26a3b21faf3a89c79a54094e9ea163
Luna Dragofelis <lunadragofelis@gmail.com>
2021-09-11 18:25:23 +0200

    First code added

---
+++ code/main.cpp
@@ -0,0 +1,15 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 11.09.2021
+ *      Author: luna
+ */
+
+#include <iostream>
+
+int main(int argc, char** argv) {
+
+	std::cout << "Hello Chausie" << std::endl;
+
+}
+
